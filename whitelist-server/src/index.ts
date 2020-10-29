edbe7afcd5aac7dc7c43ecb6b9896272c2065521
Jean <sy2803@tc.columbia.edu>
2020-10-29 09:20:01 -0400

    feat: show error msg

--- whitelist-server/src/index.ts
+++ whitelist-server/src/index.ts
@@ -1,7 +1,6 @@
 import "reflect-metadata"
 import { MikroORM } from "@mikro-orm/core"
 import { __prod__ } from "./constants"
-// import { Post } from "./entities/Post"
 import microConfig from './mikro-orm.config'
 import express from 'express'
 import {ApolloServer} from 'apollo-server-express'
@@ -12,10 +11,7 @@ import { UserResolver } from "./resolvers/user"
 import redis from 'redis'
 import session from 'express-session'
 import connectRedis from 'connect-redis'
-import { MyContext } from "./types"
-
-
- 
+import cors from 'cors'
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig)
@@ -24,7 +20,12 @@ const main = async () => {
     const app = express()
     const RedisStore = connectRedis(session)
     const redisClient = redis.createClient()
-
+    app.use(
+        cors({
+            origin: "http://localhost:3000",
+            credentials: true
+        })
+    )
     app.use(
         session({
             name: 'qid',
@@ -48,19 +49,17 @@ const main = async () => {
             resolvers: [HelloResolver, PostResolver, UserResolver],
             validate: false,
         }),
-        context: ({  req, res }): MyContext => ({ em: orm.em, req, res }),
+        context: ({  req, res }) => ({ em: orm.em, req, res }),
     })
 
-    apolloServer.applyMiddleware({ app })
+    apolloServer.applyMiddleware({
+        app,
+        cors: false
+    })
     
     app.listen(4000, () => {
         console.log('server start')
     })
-    // const post = orm.em.create(Post, {title: "my first post"})
-    // await orm.em.persistAndFlush(post)
-
-    // const posts = await orm.em.find(Post, {})
-    // console.log(posts)
 }
 
 main().catch((err) => {
