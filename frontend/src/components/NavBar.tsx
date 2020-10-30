f7e8a26855cb4f064a0702e44fab1a524d20701e
Jean <sy2803@tc.columbia.edu>
2020-10-30 14:06:06 -0400

    style: navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box, Button, Spinner } from "@chakra-ui/core"
+import { Flex, Box, Button, Spinner, Link, Heading } from "@chakra-ui/core"
 import NextLink from "next/link"
 import React from "react"
 import { useLogoutMutation, useMeQuery } from "../generated/graphql"
@@ -17,31 +17,36 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
     }
     if (data?.me) {
         return (
-            <Flex zIndex={1} position="sticky" top={0} p={4}>
-                <Box ml={"auto"} >
-                    <Flex align="center">
-                        <Box mr={4}>Hi {data.me.username} !</Box>
-                        <NextLink href="/create-post">
-                            <Button
-                                size="sm"
-                                borderRadius="full"
-                                mr={4}
-                            >
-                                Add list
-                            </Button>
-                        </NextLink>
+            <Flex zIndex={1} position="sticky" top={0} p={4} bg="blue.400" justify="space-between">
+                <Box>
+                    <NextLink href="/">
+                    <Link>
+                        <Heading>Whitelist</Heading>
+                    </Link>
+                    </NextLink>
+                </Box>
+                <Flex align="center" >
+                    <Box mr={4}>Hi {data.me.username} !</Box>
+                    <NextLink href="/create-post">
                         <Button
                             size="sm"
                             borderRadius="full"
-                            onClick={() => {
-                                logout()
-                            }}
-                            isLoading={logoutFetching}
+                            mr={4}
                         >
-                            Log out
+                            Add list
                         </Button>
-                    </Flex>
-                </Box>
+                    </NextLink>
+                    <Button
+                        size="sm"
+                        borderRadius="full"
+                        onClick={() => {
+                            logout()
+                        }}
+                        isLoading={logoutFetching}
+                    >
+                        Log out
+                    </Button>
+                </Flex>
             </Flex>
         );
     };
