4a985659587e7bcf6c7b9a94109f7b81670a100f
Jean <sy2803@tc.columbia.edu>
2020-10-29 22:21:06 -0400

    fix: navbar

---
+++ server/src/index.ts
@@ -0,0 +1,73 @@
+import "reflect-metadata"
+import { COOKIE_NAME, __prod__ } from "./constants"
+import express from 'express'
+import {ApolloServer} from 'apollo-server-express'
+import {buildSchema} from 'type-graphql'
+import { HelloResolver } from "./resolvers/hello"
+import { PostResolver } from "./resolvers/post"
+import { UserResolver } from "./resolvers/user"
+import redis from 'redis'
+import session from 'express-session'
+import connectRedis from 'connect-redis'
+import cors from 'cors'
+import { createConnection } from 'typeorm'
+import { User } from "./entities/User"
+import { Post } from "./entities/Post"
+
+const main = async () => {
+    await createConnection({
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        logging: true,
+        synchronize: true,
+        entities: [Post, User],
+    })
+
+    const app = express()
+    const RedisStore = connectRedis(session)
+    const redisClient = redis.createClient()
+    app.use(
+        cors({
+            origin: "http://localhost:3000",
+            credentials: true
+        })
+    )
+    app.use(
+        session({
+            name: COOKIE_NAME,
+            store: new RedisStore({
+                client: redisClient,
+                disableTouch: true,
+            }),
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+                httpOnly: true,
+                sameSite: 'lax',
+                secure: __prod__,
+            },
+            saveUninitialized: false,
+            secret: ';alkdsnfoiweng12341234fasdf',
+            resave: false,
+        })
+    )
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver, UserResolver],
+            validate: false,
+        }),
+        context: ({  req, res }) => ({ req, res }),
+    })
+
+    apolloServer.applyMiddleware({
+        app,
+        cors: false
+    })
+    
+    app.listen(4000, () => {
+        console.log('server start')
+    })
+}
+
+main().catch((err) => {
+    console.error(err)
+})
---
+++ server/src/resolvers/user.ts
@@ -0,0 +1,163 @@
+import { User } from "../entities/User";
+import { MyContext } from "../types";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+} from "type-graphql";
+import argon2 from "argon2";
+import { COOKIE_NAME } from "../constants";
+import { getConnection } from "typeorm";
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User
+}
+
+@Resolver()
+export class UserResolver {
+  @Query(() => User, { nullable: true })
+  me(@Ctx() { req }: MyContext) {
+    if (!req.session.userId) {
+      return null
+    }
+
+    return User.findOne(req.session.userId)
+  }
+
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { req }: MyContext
+  ): Promise<UserResponse> {
+    if (options.username.length <= 1) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "username should be longer than 1 character",
+          },
+        ],
+      };
+    }
+
+    if (options.password.length <= 1) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "password should be longer than 1 character",
+          },
+        ],
+      };
+    }
+
+    const hashedPassword = await argon2.hash(options.password);
+    let user
+
+    try {
+      const result = await getConnection()
+        .createQueryBuilder()
+        .insert()
+        .into(User)
+        .values({
+          username: options.username,
+          password: hashedPassword,
+        })
+        .returning("*")
+        .execute()
+      console.log("result: ", result)
+      user = result.raw[0]
+    } catch (err) {
+      console.log("error: ", err)
+      if (err.code === "23505") {
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "username has been already taken",
+            },
+          ],
+        };
+      }
+    }
+
+    // after storing user id session and creating a cookie,
+    // it keeeps the user logged in
+    req.session.userID = user.id;
+    return { user };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { req }: MyContext
+  ): Promise<UserResponse> {
+    const user = await User.findOne({ username: options.username });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "this username doesn't exit",
+          },
+        ],
+      };
+    }
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "incorrect password",
+          },
+        ],
+      };
+    }
+
+    req.session!.userId = user.id;
+    return { user };
+  }
+
+  @Mutation(() => Boolean)
+  logout(@Ctx() { req, res }: MyContext) {
+    return new Promise((resolve) =>
+      req.session.destroy((err) => {
+        res.clearCookie(COOKIE_NAME);
+        if (err) {
+          console.log(err);
+          resolve(false);
+          return;
+        }
+
+        resolve(true);
+      })
+    );
+  }
+}
